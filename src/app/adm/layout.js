5ef7ce10a04fb06aec31c181c7ac557b116ba2f2
Robson <robsonmrsp@gmail.com>
2023-07-06 19:43:51 -0300

    use ApplicationShell, still WIP

--- src/app/adm/layout.js
+++ src/app/adm/layout.js
@@ -3,28 +3,14 @@ import React from "react";
 // components
 
 import AdminNavbar from "@/components/Navbars/AdminNavbar.js";
-import Sidebar from "@/components/layout/Sidebar.js";
+import ApplicationShell from "@/components/layout/ApplicationShell";
 import HeaderStats from "@/components/Headers/HeaderStats.js";
 import FooterAdmin from "@/components/Footers/FooterAdmin.js";
 
 export default function Admin({ children }) {
   return (
-    <>
-      <div class="grid grid-cols-12">
-        <div class="col-span-2  ">
-          <Sidebar />
-        </div>
-        <div class="col-span-10  ">
-          <AdminNavbar />
-          <HeaderStats />
-          <div className="container mx-auto px-4 content-center">
-            <div className="px-4 md:px-10 mx-auto mb-24 w-full -m-24 pb-8">
-              {children}
-              {/* <FooterAdmin /> */}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <ApplicationShell>
+      <div>{children}</div>
+    </ApplicationShell>
   );
 }
