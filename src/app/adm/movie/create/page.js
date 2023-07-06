d36c766acaf8a7163b5093b9d1a3e1e39aec22e6
Robson <robsonmrsp@gmail.com>
2023-07-05 23:38:03 -0300

    add adm new layout

---
+++ src/app/adm/movie/create/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FormMovie from "@/components/forms/FormMovie";
+
+function CreateMovie() {
+  return (
+    <>
+      <FormMovie />
+    </>
+  );
+}
+
+export default CreateMovie;
